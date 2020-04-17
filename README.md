3445973630dcf1edbdae663fd550cc083c17feca
unknown <florescudev@gmail.com>
2020-04-17 13:43:52 -0700

    myBlogNode Setup Api file

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-"# myBlogNode" 
+"# myBlogNode"
+
+"Personal Blog Website"
+
+"NodeJS + MYSQL"
+
+"Setup v0.2"
---
+++ app/index.js
@@ -0,0 +1,27 @@
+/*Common Variables Implementation*/
+const express = require('express');
+const PORT = 8080;
+const app = express();
+
+/*Routes*/
+
+/*Welcome Route*/
+app.get('/app/index', (req, res) => {
+    res.send("Welcome Aboard Captain");
+});
+
+/*List Route*/
+app.get('/app/list', (req, res) => {
+    res.send("Welcome to List page");
+});
+
+/*About Route*/
+app.get("/app/about", (req, res) => {
+    res.send("Welcome to About Page");
+});
+
+
+/*Listening Port of the App*/
+app.listen(PORT, (req, res) => {
+    console.log("You access the app!");
+});
